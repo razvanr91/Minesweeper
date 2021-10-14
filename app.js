b0cac0dd8333a69fcf6bd4782dd66b4df1ac93aa
razvanr91 <razvan.roman91@outlook.com>
2021-10-14 14:36:16 +0100

    Create button and information elements

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 let gameBoard = document.getElementById("gameBoard");
 let gameBoardRow = document.getElementById("gameBoardRow");
+let resetButton = document.getElementById("reset")
 let cellNumber = 0;
 let maxCells = 10;
 
-for (let i = 0; i < 100; i++) {
+resetButton.addEventListener("click", resetGame);
+
+function generateBoard() {
+	for (let i = 0; i < 100; i++) {
+		let cell = createCell();
+		gameBoardRow.append(cell);
+		cellNumber++;
+		if (cellNumber === maxCells) {
+			let col = createColumn();
+			gameBoardRow.append(col);
+			cellNumber = 0;
+		}
+	}
+}
+
+function createCell() {
 	let cell = document.createElement("div");
 	cell.classList.add("cell");
 
@@ -15,12 +31,17 @@ for (let i = 0; i < 100; i++) {
 		e.preventDefault();
 	});
 
-	gameBoardRow.append(cell);
-	cellNumber++;
-	if (cellNumber === maxCells) {
-		let col = document.createElement("div");
-		col.classList.add("col");
-		gameBoardRow.append(col);
-		cellNumber = 0;
-	}
+	return cell;
 }
+
+function createColumn() {
+	let col = document.createElement("div");
+	col.classList.add("col");
+	return col;
+}
+
+function resetGame() {
+	location.reload();
+}
+
+generateBoard();
