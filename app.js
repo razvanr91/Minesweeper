b68ad668812e249319edfbf3378ddb4d4df0df72
razvanr91 <razvan.roman91@outlook.com>
2021-10-15 13:39:37 +0100

    Functionality update 1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let gameBoard = document.getElementById("gameBoard");
 let gameBoardRow = document.getElementById("gameBoardRow");
 let resetButton = document.getElementById("reset");
+let gameOver = false;
 let cellNumber = 0;
 let maxCellsRow = 10;
 let totalCells = 100;
@@ -9,6 +10,7 @@ let bombsArray = generateRandomNumberArray(numberOfBombs);
 let gameArray = [];
 
 resetButton.addEventListener("click", resetGame);
+console.log(bombsArray);
 
 function generateBoard() {
 	for (let i = 0; i < totalCells; i++) {
@@ -27,17 +29,12 @@ function createCell(id) {
 	let cell = document.createElement("div");
 	cell.classList.add("cell", "text-center", "align-middle");
 	cell.setAttribute("id", id);
+	let rightWallCell = id % maxCellsRow === maxCellsRow - 1;
+	let leftWallCell = id % maxCellsRow === 0;
 
 	// Add event for regular click
 	cell.addEventListener("click", (e) => {
-		if (bombsArray.includes(id) && cell.innerHTML === "") {
-			cell.innerHTML = `<i class="fas fa-bomb fs-1 mt-1"></i>`;
-		} else if (cell.innerHTML === "") {
-			// Right wall
-			cell.innerHTML = id % 10 === 10 - 1;
-			// Left wall
-			cell.innerHTML = id % 10 === 0;
-		}
+		clickCell(cell);
 	});
 
 	// Add event for right click
@@ -50,7 +47,33 @@ function createCell(id) {
 	return cell;
 }
 
-function checkForBombs(id) {}
+function clickCell(cell) {
+	let cellId = parseInt(cell.id);
+	if (checkForBombs(cell.id) && !gameOver) {
+		cell.innerHTML = `<i class="fas fa-bomb fs-1 mt-1"></i>`;
+		// return (gameOver = true);
+	}
+	cell.classList.add("clicked-cell");
+	if (!checkForBombs(cellId + 1)) {
+		clickCell(document.getElementById(cellId + 1));
+	} else {
+		cell.innerHTML = "1";
+	}
+}
+
+function checkCell(id) {
+	let rightWallCell = id % maxCellsRow === maxCellsRow - 1;
+	let leftWallCell = id % maxCellsRow === 0;
+
+	if(leftWallCell && id < 9) {
+		clickCell(id + 1);
+	}
+
+}
+
+function checkForBombs(id) {
+	return bombsArray.includes(Number.parseInt(id));
+}
 
 function createColumn() {
 	let col = document.createElement("div");
